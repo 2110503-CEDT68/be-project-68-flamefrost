f29048a72df0ff1e3c4d95e3aef1089a24d2bde9
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-02-27 09:32:13 +0700

    chore: remove all references to sql related code

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(express.json());
 
-const albums = require("./routes/albums");
-app.use("/albums", albums);
-
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
