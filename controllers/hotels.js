e62a6bff521061e1b4d5e8389221f1d0f0669245
kaikub <thunwa.pungtam@gmail.com>
2026-03-03 15:35:06 +0700

    fix statsu code

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -83,7 +83,7 @@ exports.getHotel = async (req, res, next) => {
     const hotel = await Hotel.findById(req.params.id);
 
     if (!hotel) {
-      return res.status(400).json({ success: false });
+      return res.status(404).json({ success: false });
     }
 
     res.status(200).json({ success: true, data: hotel });
